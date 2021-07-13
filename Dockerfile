0ba1843b0855bca5579a228f43e2ff759395a289
clairenjit369 <85318380+clairenjit369@users.noreply.github.com>
2021-07-12 21:07:39 -0400

    update

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.9
 
 ADD . .
 
+RUN pip install --upgrade pip
 RUN pip install numpy
 
 
--- main.py
+++ main.py
@@ -1,16 +1,94 @@
-# This is a sample Python script.
+from Calculator.Calculator import Calculator
+from Statistics.Statistics import Statistics
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cal = Calculator()
+stats = Statistics()
+
+try:
+    while True:
+        print("Choose an option from the following:")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Square")
+        print("6. Square Root")
+        print("7. Mean")
+        print("8. Median")
+        print("9. Mode")
+        print("10. Standard Deviation")
+        print("11. Variance")
+
+        option = int(input().strip())
+
+        if option == 1:
+            print("Enter number 1:")
+            a = int(input().strip())
+
+            print("Enter number 2:")
+            b = int(input().strip())
+            print("Result is", cal.add(a, b))
+        elif option == 2:
+            print("Enter number 1:")
+            a = int(input().strip())
+
+            print("Enter number 2:")
+            b = int(input().strip())
+            print("Result is", cal.subtract(a, b))
+        elif option == 3:
+            print("Enter number 1:")
+            a = int(input().strip())
+
+            print("Enter number 2:")
+            b = int(input().strip())
+            print(cal.multiply(a, b))
+        elif option == 4:
+            print("Enter number 1:")
+            a = int(input().strip())
+
+            print("Enter number 2:")
+            b = int(input().strip())
+            print("Result is", cal.divide(a, b))
+        elif option == 5:
+            print("Enter number 1:")
+            a = int(input().strip())
+            print("Result is", cal.square(a))
+        elif option == 6:
+            print("Enter number 1:")
+            a = int(input().strip())
+            print("Result is", cal.sqrt(a))
+        elif option == 7:
+            print("Enter number of numbers:")
+            count = int(input().strip())
+            data = [int(input().strip()) for i in range(count)]
+            print("Result is", stats.get_mean(data))
+        elif option == 8:
+            print("Enter number of numbers:")
+            count = int(input().strip())
+            data = [int(input().strip()) for i in range(count)]
+            print("Result is", stats.get_median(data))
+        elif option == 9:
+            print("Enter number of numbers:")
+            count = int(input().strip())
+            data = [int(input().strip()) for i in range(count)]
+            print("Result is", stats.get_mode(data))
+        elif option == 10:
+            print("Enter number of numbers:")
+            count = int(input().strip())
+            data = [int(input().strip()) for i in range(count)]
+            print("Result is", stats.get_stdev(data))
+        elif option == 11:
+            print("Enter number of numbers:")
+            count = int(input().strip())
+            data = [int(input().strip()) for i in range(count)]
+            print("Result is", stats.get_variance(data))
+
+        print("Exit :: Press 1 || Continue :: Press 0")
+        exit_pressed = int(input().strip())
+        if exit_pressed == 1:
+            print("Bye!")
+            break;
+except:
+    print("Invalid Literal :: Should be an Integer")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
