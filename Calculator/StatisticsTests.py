c74b9dcdf8f21383d9f235f85f78c67f4a1e8459
clairenjit369 <85318380+clairenjit369@users.noreply.github.com>
2021-07-14 01:34:42 -0400

    Update StatisticsTests.py

--- Calculator/StatisticsTests.py
+++ Calculator/StatisticsTests.py
@@ -24,30 +24,22 @@ class MyTestCase(unittest.TestCase):
     def test_mean_result(self):
         self.assertEqual(self.statistics.get_mean(self.integerRandomDataF), stats.mean(self.integerRandomDataF))
         self.assertEqual(self.statistics.get_mean(self.decimalRandomData), stats.mean(self.decimalRandomData))
-        print("Mean passed")
 
     def test_median_result(self):
         self.assertEqual(self.statistics.get_median(self.integerRandomDataF), stats.median(self.integerRandomDataF))
-        print("Median for Integer Random List passed")
         self.assertEqual(self.statistics.get_median(self.decimalRandomData), stats.median(self.decimalRandomData))
-        print("Median for Decimal Random List passed")
 
     def test_mode_result(self):
         self.assertTrue(stats.mode(self.integerRandomDataT.tolist()) in self.statistics.get_mode(self.integerRandomDataT.tolist()))
-        print("Mode for Integer Random List passed")
-        print("Mode for Decimal Random List passed")
 
     def test_variance_result(self):
         self.assertEqual(self.statistics.get_variance(self.integerRandomDataF), stats.variance(self.integerRandomDataF))
-        print("Variance for Integer Random List passed")
         self.assertEqual(self.statistics.get_variance(self.decimalRandomData), stats.variance(self.decimalRandomData))
-        print("Variance for Decimal Random List passed")
+
 
     def test_stdev_result(self):
         self.assertEqual(self.statistics.get_stdev(self.integerRandomDataF), stats.stdev(self.integerRandomDataF))
-        print("Standard Deviation for Integer Random List passed")
         self.assertEqual(self.statistics.get_stdev(self.decimalRandomData), stats.stdev(self.decimalRandomData))
-        print("Standard Deviation for Decimal Random List passed")
 
 
 if __name__ == '__main__':
